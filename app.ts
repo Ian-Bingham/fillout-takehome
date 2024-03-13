c1b2ba8a9fb24650483606680d8ae82ee13e19de
Ian Bingham <ianbingham24@gmail.com>
2024-03-12 22:26:42 -0700

    fix: add global error handler

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import * as FormRoutes from "./src/routes/forms.routes";
 
 require("dotenv").config();
@@ -7,6 +8,13 @@ const app = express();
 const port = 3000;
 
 app.use("/v1/api/forms", FormRoutes.router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error("error: ", err.stack);
+	res
+		.status(res.statusCode || 500)
+		.json({ message: err.message, stack: err.stack });
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
--- src/controllers/forms.controllers.ts
+++ src/controllers/forms.controllers.ts
@@ -1,15 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as FormsService from "../services/forms.service";
 import { PathParams, QueryParams } from "../types";
 
 export const getFilteredResponses = async (
 	req: Request<PathParams, {}, {}, QueryParams>,
-	res: Response
+	res: Response,
+	next: NextFunction
 ): Promise<void> => {
-	const response = await FormsService.getFilteredResponses({
-		formId: req.params.formId,
-		query: req.query,
-	});
+	try {
+		const response = await FormsService.getFilteredResponses({
+			formId: req.params.formId,
+			query: req.query,
+		});
 
-	res.send(response);
+		res.send(response);
+	} catch (err) {
+		next(err);
+	}
 };
